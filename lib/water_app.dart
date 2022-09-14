158c39b1bb6311b76c64a99b8e90cfc0b9b0c4f0
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-09-14 19:37:35 +0300

    remove debug banner

--- lib/water_app.dart
+++ lib/water_app.dart
@@ -12,6 +12,7 @@ class _WaterAppState extends State<WaterApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: SafeArea(child: LoginScreen()),
     );
   }
