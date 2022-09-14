c446aa666f8a2f094d4e36ff597089de49f527e3
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-09-14 20:00:44 +0300

    divided login screen and login layout

---
+++ lib/presentation/screens/login_screen/login_layout.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:water_tracker/presentation/widgets/logo_widget.dart';
+
+class LoginLayout extends StatefulWidget {
+  const LoginLayout({Key? key}) : super(key: key);
+
+  @override
+  State<LoginLayout> createState() => _LoginLayoutState();
+}
+
+class _LoginLayoutState extends State<LoginLayout> {
+  static const paddingLeft = 20.0;
+  static const paddingTop = 30.0;
+  static const paddingRight = 20.0;
+  static const paddingBottom = 8.0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(
+            paddingLeft, paddingTop, paddingRight, paddingBottom),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            LogoWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/screens/login_screen/login_screen.dart
+++ lib/presentation/screens/login_screen/login_screen.dart
@@ -1,32 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:water_tracker/presentation/widgets/logo_widget.dart';
+import 'package:water_tracker/presentation/screens/login_screen/login_layout.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  static const paddingLeft = 20.0;
-  static const paddingTop = 30.0;
-  static const paddingRight = 20.0;
-  static const paddingBottom = 8.0;
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(
-            paddingLeft, paddingTop, paddingRight, paddingBottom),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            LogoWidget(),
-          ],
-        ),
-      ),
-    );
+    return const LoginLayout();
   }
 }
-// TODO: Divide screen and screen layout
