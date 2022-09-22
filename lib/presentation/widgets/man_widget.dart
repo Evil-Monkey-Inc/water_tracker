8bc5fa6601a728c9ef03d21bd1ba649270fc1626
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-09-22 13:56:27 +0300

    deleted manWidth

--- lib/presentation/widgets/man_widget.dart
+++ lib/presentation/widgets/man_widget.dart
@@ -5,14 +5,12 @@ class ManWidget extends StatelessWidget {
 
   static const manImagePath = "assets/images/Man1.png";
   static const manHeight = 241.0;
-  static const manWidth = 236.0;
 
   @override
   Widget build(BuildContext context) {
     return Image.asset(
       manImagePath,
       height: manHeight,
-      width: manWidth,
       fit: BoxFit.fitHeight,
     );
   }
