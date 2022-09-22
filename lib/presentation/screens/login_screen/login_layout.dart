e05aa52d3ca1271a395979bba0b69d1f1e84af8b
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-09-22 13:27:58 +0300

    remove padding and add sizedbox for vertical and padding for horizontal

--- lib/presentation/screens/login_screen/login_layout.dart
+++ lib/presentation/screens/login_screen/login_layout.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:water_tracker/presentation/widgets/login_button_widget.dart';
 import 'package:water_tracker/presentation/widgets/logo_form.dart';
 import 'package:water_tracker/presentation/widgets/logo_widget.dart';
 import 'package:water_tracker/presentation/widgets/man_widget.dart';
@@ -12,22 +11,20 @@ class LoginLayout extends StatefulWidget {
 }
 
 class _LoginLayoutState extends State<LoginLayout> {
-  static const paddingLeft = 20.0;
-  static const paddingTop = 30.0;
-  static const paddingRight = 20.0;
-  static const paddingBottom = 8.0;
+  static const paddingHorizontal = 20.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.fromLTRB(
-            paddingLeft, paddingTop, paddingRight, paddingBottom),
+        padding: const EdgeInsets.symmetric(horizontal: paddingHorizontal),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const LogoWidget(),
-              const SizedBox(height: 20),
+            children: const [
+              SizedBox(height: 30),
+              LogoWidget(),
+              SizedBox(height: 20),
               ManWidget(),
               LogoForm(),
             ],
