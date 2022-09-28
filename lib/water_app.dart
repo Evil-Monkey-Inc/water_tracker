2159f047333e6fbad39301f2f0d5c3f84ffedbbd
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-09-28 15:05:59 +0300

    moved borders values to variables

--- lib/water_app.dart
+++ lib/water_app.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:water_tracker/presentation/screens/login_screen/login_screen.dart';
 
@@ -9,10 +10,53 @@ class WaterApp extends StatefulWidget {
 }
 
 class _WaterAppState extends State<WaterApp> {
+  final focusedBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10.0),
+    borderSide: const BorderSide(),
+  );
+
+  final focusedErrorBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10.0),
+    borderSide: const BorderSide(color: Colors.red),
+  );
+
+  final unFocusedBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10.0),
+    borderSide: BorderSide.none,
+  );
+
+  final disabledBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10.0), borderSide: BorderSide.none);
+
+  final errorBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10.0),
+      borderSide: const BorderSide(color: Colors.red));
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SafeArea(child: LoginScreen()),
+    return MaterialApp(
+      scrollBehavior: const CupertinoScrollBehavior(),
+      theme: ThemeData(
+        primaryColor: const Color(0xFF292D32),
+        focusColor: const Color(0xFF292D32),
+        inputDecorationTheme: InputDecorationTheme(
+          focusedBorder: focusedBorder,
+          focusedErrorBorder: focusedErrorBorder,
+          enabledBorder: unFocusedBorder,
+          disabledBorder: disabledBorder,
+          border: focusedBorder,
+          errorBorder: errorBorder,
+          filled: true,
+          fillColor: const Color(0xFFF2F4F8),
+          labelStyle: const TextStyle(
+            color: Color(0xFF292D32),
+          ),
+        ),
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Color(0xFF292D32),
+        ),
+      ),
+      home: const SafeArea(child: LoginScreen()),
     );
   }
 }
