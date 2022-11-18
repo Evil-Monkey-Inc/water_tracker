ce2fab3910da97ab7730f0c32592bd7b595a687d
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-11-18 20:51:24 +0200

    sated registration screen and layout

---
+++ lib/presentation/screens/login_screen/registration_layout..dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RegistrationLayout extends StatelessWidget {
+  const RegistrationLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Registration screen');
+  }
+}
