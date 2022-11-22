55acd78e0afe7a8876ba44a3b086997272df960a
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-11-22 14:23:30 +0200

    refactored widget

--- lib/presentation/widgets/privacy_policy_and_terms_widget.dart
+++ lib/presentation/widgets/privacy_policy_and_terms_widget.dart
@@ -5,39 +5,43 @@ import 'package:water_tracker/custom_theme.dart';
 class PrivacyPolicyAndTermsWidget extends StatelessWidget {
   const PrivacyPolicyAndTermsWidget({Key? key}) : super(key: key);
 
-  static const privacyPolicyText = 'By signing in, you agree to our\t';
+  static const spaceTextSpan = TextSpan(text: ' ', style: TextStyle(fontSize: 16, color: Colors.black));
+  // todo: move to theme
+  static const privacyPolicyText = 'By signing in, you agree to our';
   static const terms = 'Terms';
-  static const and = '\tand\t';
+  static const and = 'and';
   static const privacyPolicy = 'Privacy Policy.';
+  static const paddingPrivacyPolicyTermsWidget = EdgeInsets.all(16.0);
   // Todo: refactored widget and add to localization
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: CustomTheme.paddingPrivacyPolicyTermsWidget,
+      padding: paddingPrivacyPolicyTermsWidget,
       child: Text.rich(
         TextSpan(
           text: privacyPolicyText,
           style: CustomTheme.restText,
           children: <TextSpan>[
+            spaceTextSpan,
             TextSpan(
               text: terms,
               style: CustomTheme.termsProperty,
               recognizer: TapGestureRecognizer()..onTap = () {},
               // TODO:  code to open / launch terms of service link here
             ),
-            TextSpan(
+            spaceTextSpan,
+            const TextSpan(
               text: and,
-              style: const TextStyle(fontSize: 16, color: Colors.black),
-              children: <TextSpan>[
-                TextSpan(
-                  text: privacyPolicy,
-                  style: CustomTheme.privacyPolicyTextProperty,
-                  recognizer: TapGestureRecognizer()
-                    ..onTap = () {
-                      // TODO:  code to open / launch terms of service link here
-                    },
-                ),
-              ],
+              style: TextStyle(fontSize: 16, color: Colors.black), // todo: move to theme
+            ),
+            spaceTextSpan,
+            TextSpan(
+              text: privacyPolicy,
+              style: CustomTheme.privacyPolicyTextProperty,
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  // TODO:  code to open / launch terms of service link here
+                },
             ),
           ],
         ),
