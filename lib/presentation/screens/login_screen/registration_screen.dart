023d2218c1b04d3eb6eb08702e11c346fea14418
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-11-27 08:36:32 +0200

    wrap screen to SafeArea

--- lib/presentation/screens/login_screen/registration_screen.dart
+++ lib/presentation/screens/login_screen/registration_screen.dart
@@ -7,6 +7,6 @@ class RegistrationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const RegistrationLayout();
+    return const SafeArea(child: RegistrationLayout());
   }
 }
