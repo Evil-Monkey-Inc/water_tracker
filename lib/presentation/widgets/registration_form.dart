4c907e3471dbe7019c5bd65355fb4f0aee73650a
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-11-27 08:39:12 +0200

    sat down registration form

---
+++ lib/presentation/widgets/registration_form.dart
@@ -0,0 +1,65 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:water_tracker/custom_theme.dart';
+import 'package:water_tracker/form_validators.dart';
+import 'package:water_tracker/generated/locale_keys.g.dart';
+import 'package:water_tracker/presentation/widgets/input_field_widget.dart';
+import 'package:water_tracker/presentation/widgets/login_button_widget.dart';
+
+class RegisterForm extends StatefulWidget {
+  const RegisterForm({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterForm> createState() => _MyLogFormWidgetState();
+}
+
+class _MyLogFormWidgetState extends State<RegisterForm> {
+  static const spacer = SizedBox(height: 28);
+  final formKey = GlobalKey<FormState>();
+  final emailNode = FocusNode();
+
+  @override
+  void initState() {
+    emailNode.addListener(() => setState(() {}));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        children: [
+          const Text(
+            'Start your journey.',
+            style: CustomTheme.greetingsProperty,
+          ),
+          spacer,
+          InputFieldWidget(
+            labelText: LocaleKeys.enter_your_email.tr(),
+            validator: FormValidators.emailValidator,
+          ),
+          spacer,
+          InputFieldWidget(
+            labelText: LocaleKeys.enter_password.tr(),
+            validator: FormValidators.passwordValidator,
+          ),
+          spacer,
+          InputFieldWidget(
+            // controller:  ,
+            labelText: 'Re-enter your password',
+            validator: FormValidators.repeatPasswordValidator, // todo: change validator
+          ),
+          spacer,
+          CustomButton(
+            onPressed: () {
+              // TODO: call something on validation
+              if (formKey.currentState!.validate()) {}
+            },
+            text: 'Sing Up',
+          ),
+        ],
+      ),
+    );
+  }
+}
