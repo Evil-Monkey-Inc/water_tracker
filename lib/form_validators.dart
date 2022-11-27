96da2ae64cb3c868616e2934804310916aec18cb
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-11-27 15:16:24 +0200

    added new validator function

--- lib/form_validators.dart
+++ lib/form_validators.dart
@@ -1,21 +1,20 @@
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/material.dart';
 import 'package:water_tracker/generated/locale_keys.g.dart';
 
-typedef ValidatorFunction = String? Function(String?);
+typedef SingleValidatorFunction = String? Function(String?);
+typedef DoubleValidatorFunction = String? Function(String?, String?);
 
 class FormValidators {
-  final TextEditingController password = TextEditingController();
   static final _emailRegexp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w]{2,4}');
   static final _passwordRegexp = RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z]).{8,}$');
 
-  static ValidatorFunction emailValidator = (value) {
+  static SingleValidatorFunction emailValidator = (value) {
     if (value == null || value.isEmpty) return LocaleKeys.enter_your_email.tr();
     if (!_emailRegexp.hasMatch(value)) return LocaleKeys.please_enter_correct_email.tr();
     return null;
   };
 
-  static ValidatorFunction passwordValidator = (value) {
+  static SingleValidatorFunction passwordValidator = (value) {
     if (value == null || value.isEmpty) return LocaleKeys.please_enter_password.tr();
     if (!_passwordRegexp.hasMatch(value)) {
       return LocaleKeys.invalid_password.tr();
@@ -24,12 +23,8 @@ class FormValidators {
     }
   };
 
-  static ValidatorFunction repeatPasswordValidator = (value) {
-    if (value == null || value.isEmpty) return LocaleKeys.please_enter_password.tr();
-    if (!_passwordRegexp.hasMatch(value)) {
-      return LocaleKeys.invalid_password.tr();
-    } else {
-      return null;
-    }
+  static DoubleValidatorFunction repeatPasswordValidator = (value1, value2) {
+    if (value1 == null || value1.isEmpty) return LocaleKeys.please_enter_password.tr();
+    return value1 == value2 ? null : "Passwords do not match";
   };
 }
