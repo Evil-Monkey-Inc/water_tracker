4c7e9ad1c5717b3a095b6975e8ea6cc25ca14e08
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-11-29 13:08:54 +0200

    WT-16: refactored code, and put away paddings

--- lib/presentation/widgets/logo_widget.dart
+++ lib/presentation/widgets/logo_widget.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:water_tracker/assets.dart';
-import 'package:water_tracker/custom_theme.dart';
 
 class LogoWidget extends StatelessWidget {
   const LogoWidget({Key? key}) : super(key: key);
 
+  static const logoWidth = 16.0;
+  static const logoTextWidth = 87.5;
   static const padding = 8.0;
 
   @override
@@ -15,12 +16,12 @@ class LogoWidget extends StatelessWidget {
       children: [
         SvgPicture.asset(
           Assets.logoImagePath,
-          width: CustomTheme.logoWidth,
+          width: logoWidth,
         ),
         const SizedBox(width: padding),
         SvgPicture.asset(
           Assets.logoTextImage,
-          width: CustomTheme.logoTextWidth,
+          width: logoTextWidth,
         ),
       ],
     );
