3bcfb26647846dd53e44a4581cee69e59c2ba4b7
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-03 01:49:20 +0200

    connected bloc provider to the RegistrationLayout

---
+++ lib/presentation/screens/registration_screen/registration_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:water_tracker/data/repository/repository.dart';
+import 'package:water_tracker/get_it.dart';
+import 'package:water_tracker/presentation/screens/registration_screen/bloc/registration_bloc.dart';
+import 'package:water_tracker/presentation/screens/registration_screen/registration_layout.dart';
+
+class RegistrationScreen extends StatelessWidget {
+  const RegistrationScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: BlocProvider<RegistrationBloc>(
+      create: (context) => RegistrationBloc(getIt<Repository>()),
+      child: const RegistrationLayout(),
+    ));
+  }
+}
