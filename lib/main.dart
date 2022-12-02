9d22f59ff7c8312da1a77c6ec9dce8f70521f7cd
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-03 01:46:58 +0200

    connected setupServicesLocator to the main

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'package:water_tracker/water_app.dart';
 import 'generated/codegen_loader.g.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'get_it.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform, // todo: what is this?
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  setupServicesLocator();
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   EasyLocalization.logger.enableBuildModes = [];
