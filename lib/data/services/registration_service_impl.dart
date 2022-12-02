bdd420375e6e4a06d91c0ad68d03d83db7199f8a
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-03 01:43:11 +0200

    WT-21: created registration service

---
+++ lib/data/services/registration_service_impl.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:water_tracker/data/services/registration_service.dart';
+
+class RegistrationServiceImpl extends RegistrationService {
+  final credential = FirebaseAuth.instance;
+
+  @override
+  Future<void> registerUser(String password, String email) async {
+    credential.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+}
