9bdc3cf4a83d54522bd6d9ae0ef408aeb0a9e8e3
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-03 01:46:40 +0200

    created get it, and setupServicesLocator where our dependencies are stored

---
+++ lib/get_it.dart
@@ -0,0 +1,15 @@
+import 'package:get_it/get_it.dart';
+import 'package:water_tracker/data/repository/repository.dart';
+import 'package:water_tracker/data/repository/repository_impl..dart';
+import 'package:water_tracker/data/services/registration_service.dart';
+import 'package:water_tracker/data/services/registration_service_impl.dart';
+
+final getIt = GetIt.instance;
+
+void setupServicesLocator() {
+  final registrationService = RegistrationServiceImpl();
+
+  getIt.registerSingleton<RegistrationService>(registrationService);
+
+  getIt.registerSingleton<Repository>(RepositoryImpl(registrationService));
+}
