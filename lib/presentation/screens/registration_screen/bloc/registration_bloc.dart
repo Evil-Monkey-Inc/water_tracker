a2d8e4e50b43d54c1cc0feef1f0c80a18dac081a
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-03 01:48:58 +0200

    created bloc

---
+++ lib/presentation/screens/registration_screen/bloc/registration_bloc.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:water_tracker/data/repository/repository.dart';
+import 'package:water_tracker/presentation/screens/registration_screen/bloc/registration_events..dart';
+import 'package:water_tracker/presentation/screens/registration_screen/bloc/registration_states..dart';
+
+class RegistrationBloc extends Bloc<RegistrationEvent, RegistrationState> {
+  Repository repository;
+
+  RegistrationBloc(this.repository) : super(InitialRegistrationState()) {
+    on<RegistrationUserEvent>((event, emit) async {
+      try {
+        repository.registerUser(event.password, event.email);
+      } catch (e) {
+        print(e);
+      }
+    });
+  }
+}
