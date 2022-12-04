4d128f229c44e8032d794b50e8777fbb6e503e78
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-04 19:05:27 +0200

    WT-22: reworked service to depend on models

--- lib/data/repository/repository_impl.dart
+++ lib/data/repository/repository_impl.dart
@@ -3,8 +3,13 @@ import 'package:water_tracker/data/services/registration_service.dart';
 
 class RepositoryImpl extends Repository {
   RepositoryImpl(this.registrationService);
+
   final RegistrationService registrationService;
 
   @override
-  Future<void> registerUser(String password, String email) => registrationService.registerUser(password, email);
+  Future<bool> registerUser(String email, String password) async {
+    final result = await registrationService.registerUser(email, password);
+    final isSuccessful = result.error == null;
+    return isSuccessful;
+  }
 }
--- lib/data/services/registration_service_impl.dart
+++ lib/data/services/registration_service_impl.dart
@@ -1,14 +1,24 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:water_tracker/data/models/errors/sign_up_exception.dart';
+import 'package:water_tracker/data/models/responses/sign_up_result.dart';
+import 'package:water_tracker/data/models/user.dart';
 import 'package:water_tracker/data/services/registration_service.dart';
 
 class RegistrationServiceImpl extends RegistrationService {
-  final credential = FirebaseAuth.instance;
+  final firebaseAuth = auth.FirebaseAuth.instance;
 
   @override
-  Future<void> registerUser(String password, String email) async {
-    credential.createUserWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
+  Future<SignUpResult> registerUser(String email, String password) async {
+    SignUpException? error;
+    auth.UserCredential? credential;
+    try {
+      credential = await firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+    } on auth.FirebaseAuthException catch (e) {
+      error = SignUpException.fromFirebaseAuth(e);
+    }
+    User? user;
+    if (credential != null) user = User(email);
+    final result = SignUpResult(user, error);
+    return result;
   }
 }
