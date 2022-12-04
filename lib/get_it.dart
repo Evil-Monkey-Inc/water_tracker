069bdbfd3c1a4255f23422160653f100553d5848
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-04 22:07:51 +0200

    refactored

--- lib/get_it.dart
+++ lib/get_it.dart
@@ -1,8 +1,8 @@
 import 'package:get_it/get_it.dart';
 import 'package:water_tracker/data/repository/repository.dart';
-import 'package:water_tracker/data/repository/repository_impl..dart';
+import 'package:water_tracker/data/repository/repository_impl.dart';
 import 'package:water_tracker/data/services/registration_service.dart';
-import 'package:water_tracker/data/services/registration_service_impl.dart';
+import 'package:water_tracker/data/services/registration_service_firebase_impl.dart';
 
 final getIt = GetIt.instance;
 
