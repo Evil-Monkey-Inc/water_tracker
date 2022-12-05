1dbf58ab09076aebd561166b59ff37bbdb27805d
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-05 12:35:45 +0200

    WT-21: added new states for RegistrationState

--- lib/presentation/screens/registration_screen/bloc/registration_state.dart
+++ lib/presentation/screens/registration_screen/bloc/registration_state.dart
@@ -2,11 +2,9 @@ abstract class RegistrationState {}
 
 class InitialRegistrationState extends RegistrationState {}
 
-// class LoadingRegistrationState extends RegistrationState {}
+class LoadingRegistrationState extends RegistrationState {}
 
-// class LoadedRegistrationState extends RegistrationState {}
-
-// class SuccessfullyRegistrationState extends RegistrationState {}
+class SuccessfullyRegistrationState extends RegistrationState {}
 
 class ErrorRegistrationState extends RegistrationState {
   ErrorRegistrationState(this.exception);
