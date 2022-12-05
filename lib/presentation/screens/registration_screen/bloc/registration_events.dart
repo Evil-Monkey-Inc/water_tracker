f32bbbfdd1e0ae454dfc0d8716ff535e4236f873
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-05 12:37:20 +0200

    WT-21: deleted event

--- lib/presentation/screens/registration_screen/bloc/registration_events.dart
+++ lib/presentation/screens/registration_screen/bloc/registration_events.dart
@@ -1,7 +1,5 @@
 abstract class RegistrationEvent {}
 
-class LoadedRegistrationScreenEvent extends RegistrationEvent {}
-
 class CreateUserEvent extends RegistrationEvent {
   CreateUserEvent(this.password, this.email);
   final String email;
