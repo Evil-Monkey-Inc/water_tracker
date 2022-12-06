8b354ac1202b76c8f692eb9c258c01c73f566d32
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-06 23:20:50 +0200

    WT-13: change navigate to SignUpScreen

--- lib/presentation/widgets/not_have_account_widget.dart
+++ lib/presentation/widgets/not_have_account_widget.dart
@@ -2,7 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:water_tracker/custom_theme.dart';
 import 'package:water_tracker/generated/locale_keys.g.dart';
-import 'package:water_tracker/presentation/screens/sing_in_screen/sign_in_screen.dart';
+import 'package:water_tracker/presentation/screens/sign_up_screen/sign_up_screen.dart';
 
 class NotHaveAccount extends StatelessWidget {
   const NotHaveAccount({Key? key}) : super(key: key);
@@ -31,7 +31,7 @@ class NotHaveAccount extends StatelessWidget {
                 ],
               ),
             ),
-            onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => const SignInScreen())),
+            onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => const SignUpScreen())),
           ),
         ],
       ),
