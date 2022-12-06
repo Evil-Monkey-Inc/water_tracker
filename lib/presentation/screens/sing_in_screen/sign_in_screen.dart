49faf3e758ba88859b59513597dd6518a52f670f
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-06 22:45:23 +0200

    WT-13: wrapped layout with BlocProvider

--- lib/presentation/screens/sing_in_screen/sign_in_screen.dart
+++ lib/presentation/screens/sing_in_screen/sign_in_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:water_tracker/data/repository/repository.dart';
+import 'package:water_tracker/get_it.dart';
+import 'package:water_tracker/presentation/screens/sing_in_screen/bloc/sign_in_bloc.dart';
 import 'package:water_tracker/presentation/screens/sing_in_screen/sign_in_layout.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -6,6 +10,6 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SignInLayout();
+    return BlocProvider<SignInBloc>(create: (context) => SignInBloc(getIt<Repository>()), child: const SignInLayout());
   }
 }
