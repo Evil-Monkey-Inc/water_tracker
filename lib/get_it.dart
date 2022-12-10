b0aee79b9259fee4220486265b4a02b28e9b391d
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-11 00:47:33 +0200

    WT-67: added storage to DI

--- lib/get_it.dart
+++ lib/get_it.dart
@@ -1,13 +1,17 @@
 import 'package:get_it/get_it.dart';
 import 'package:water_tracker/data/repository/repository.dart';
 import 'package:water_tracker/data/repository/repository_impl.dart';
-import 'package:water_tracker/data/services/authentication_service.dart';
-import 'package:water_tracker/data/services/authentication_service_firebase_impl.dart';
+import 'package:water_tracker/data/services/authentication_service/authentication_service.dart';
+import 'package:water_tracker/data/services/authentication_service/authentication_service_firebase_impl.dart';
+import 'package:water_tracker/data/services/storage_service/storage_service.dart';
+import 'package:water_tracker/data/services/storage_service/storage_service_shered_preff_impl.dart';
 
 final getIt = GetIt.instance;
 
 void setupServicesLocator() {
-  getIt.registerSingleton<AuthenticationService>(AuthenticationServiceFirebaseImpl());
-
-  getIt.registerSingleton<Repository>(RepositoryImpl(getIt<AuthenticationService>()));
+  final authService = AuthenticationServiceFirebaseImpl();
+  final storageService = StorageServiceImpl();
+  getIt.registerSingleton<AuthenticationService>(authService);
+  getIt.registerSingleton<StorageService>(storageService);
+  getIt.registerSingleton<Repository>(RepositoryImpl(authService, storageService));
 }
