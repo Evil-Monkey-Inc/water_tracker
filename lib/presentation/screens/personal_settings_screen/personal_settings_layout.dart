540a98d2c86b054011de10b672c12aada126a6fe
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-11 18:28:52 +0200

    WT-67: change gender path

--- lib/presentation/screens/personal_settings_screen/personal_settings_layout.dart
+++ lib/presentation/screens/personal_settings_screen/personal_settings_layout.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:water_tracker/data/models/gender.dart';
 import 'package:water_tracker/generated/locale_keys.g.dart';
 import 'package:water_tracker/presentation/widgets/custom_button.dart';
 import 'package:water_tracker/presentation/widgets/name_and_skip_widget.dart';
