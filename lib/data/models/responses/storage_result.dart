a2679976cc166a267ccc4b1caa07fd447eef4fad
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-12 17:23:31 +0200

    WT-67: deleted GeneralInfoSavingResult

--- lib/data/models/responses/storage_result.dart
+++
@@ -1,8 +0,0 @@
-import 'package:water_tracker/data/models/user.dart';
-
-class GeneralInfoSavingResult {
-  final User? user;
-  final Object? error;
-
-  GeneralInfoSavingResult(this.user, this.error);
-}
