0d2001b840e57913ffb3b804914ef35ac7f2b239
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-12 20:23:41 +0200

    WT-67: make GeneralInfoResult to nullsafety

--- lib/data/models/responses/general_info_result.dart
+++ lib/data/models/responses/general_info_result.dart
@@ -2,6 +2,6 @@ import 'package:water_tracker/data/models/user_settings.dart';
 
 class GeneralInfoResult {
   GeneralInfoResult(this.userSettings, this.error);
+  final Object? error;
   final UserSettings? userSettings;
-  final Object error;
 }
