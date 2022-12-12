7ee0010f4c2c06fc33c70a090f7049abb125994b
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-12 04:09:28 +0200

    pass style from theme

--- lib/presentation/screens/personal_settings_screen/personal_settings_layout.dart
+++ lib/presentation/screens/personal_settings_screen/personal_settings_layout.dart
@@ -1,5 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:water_tracker/custom_theme.dart';
 import 'package:water_tracker/data/models/gender.dart';
 import 'package:water_tracker/generated/locale_keys.g.dart';
 import 'package:water_tracker/presentation/widgets/custom_button.dart';
@@ -20,8 +21,8 @@ class _PersonalSettingsLayoutState extends State<PersonalSettingsLayout> {
   int? weight;
   int? age;
 
-  final sliderWeightTextProperty = Text(LocaleKeys.weight.tr(), style: TextStyle(fontSize: 20));
-  final sliderAgeTextProperty = Text(LocaleKeys.age.tr(), style: const TextStyle(fontSize: 20));
+  final sliderWeightTextProperty = Text(LocaleKeys.weight.tr(), style: CustomTheme.header);
+  final sliderAgeTextProperty = Text(LocaleKeys.age.tr(), style: CustomTheme.header);
   static const spaces = SizedBox(height: 24);
   static const paddingHorizontal = EdgeInsets.symmetric(horizontal: 24.0);
 
