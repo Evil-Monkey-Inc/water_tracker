e35dc5539e59467ac451baf4324edcc8b3937909
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-13 12:52:18 +0200

    WT-88: deleted error object

--- lib/data/models/responses/general_info_result.dart
+++ lib/data/models/responses/general_info_result.dart
@@ -1,7 +1,6 @@
 import 'package:water_tracker/data/models/user_settings.dart';
 
 class GeneralInfoResult {
-  GeneralInfoResult(this.userSettings, this.error);
-  final Object? error;
+  GeneralInfoResult(this.userSettings);
   final UserSettings? userSettings;
 }
