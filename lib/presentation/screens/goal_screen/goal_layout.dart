d25f0ca14cf42cde51937577a8423fdbd78c6a37
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-13 03:22:55 +0200

    WT-88: added navigation to the next screen

--- lib/presentation/screens/goal_screen/goal_layout.dart
+++ lib/presentation/screens/goal_screen/goal_layout.dart
@@ -1,9 +1,13 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:water_tracker/generated/locale_keys.g.dart';
 import 'package:water_tracker/presentation/widgets/name_and_skip_widget.dart';
+import 'package:water_tracker/presentation/widgets/title_settings_widget.dart';
 
 class GoalLayout extends StatelessWidget {
   const GoalLayout({Key? key}) : super(key: key);
 
+  static const spaces = SizedBox(height: 24);
   static const paddingHorizontal = EdgeInsets.symmetric(horizontal: 24.0);
 
   @override
@@ -11,8 +15,14 @@ class GoalLayout extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: paddingHorizontal,
-        child: NameAndSkipWidget(
-          onPressed: () {},
+        child: Column(
+          children: [
+            spaces,
+            NameAndSkipWidget(
+              onPressed: () {},
+            ),
+            TitleSettingWidget(LocaleKeys.what_is_your_goal.tr()),
+          ],
         ),
       ),
     );
