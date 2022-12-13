391814f8b70fd5cfe003ea7e396851e6a0695150
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-14 00:25:58 +0200

    WT-88: built widgets in the layout

--- lib/presentation/screens/goal_screen/goal_layout.dart
+++ lib/presentation/screens/goal_screen/goal_layout.dart
@@ -1,6 +1,9 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:water_tracker/generated/locale_keys.g.dart';
+import 'package:water_tracker/presentation/widgets/custom_button.dart';
+import 'package:water_tracker/presentation/widgets/goal_widget.dart';
+import 'package:water_tracker/presentation/widgets/icon_image_widget.dart';
 import 'package:water_tracker/presentation/widgets/name_and_skip_widget.dart';
 import 'package:water_tracker/presentation/widgets/title_settings_widget.dart';
 
@@ -9,6 +12,8 @@ class GoalLayout extends StatelessWidget {
 
   static const spaces = SizedBox(height: 24);
   static const paddingHorizontal = EdgeInsets.symmetric(horizontal: 24.0);
+  static const spacesBetweenGoal = SizedBox(width: 16);
+  static const paddingTop = EdgeInsets.symmetric(vertical: 8);
   static const upperFlex = 2;
   static const downFlex = 2;
 
@@ -29,6 +34,60 @@ class GoalLayout extends StatelessWidget {
               upperFlex: upperFlex,
               downFlex: downFlex,
             ),
+            spaces,
+            Wrap(
+              children: [
+                Padding(
+                  padding: paddingTop,
+                  child: GoalWidget(
+                    titleGoal: 'Drink more Water',
+                    iconImageWidget: IconImageWidget.bottleIconWay(),
+                  ),
+                ),
+                spacesBetweenGoal,
+                Padding(
+                  padding: paddingTop,
+                  child: GoalWidget(
+                    titleGoal: 'Lose weight',
+                    iconImageWidget: IconImageWidget.weightIconWay(),
+                  ),
+                ),
+                spacesBetweenGoal,
+                Padding(
+                  padding: paddingTop,
+                  child: GoalWidget(
+                    titleGoal: 'Shine skin',
+                    iconImageWidget: IconImageWidget.shineSkinIconWay(),
+                  ),
+                ),
+                spacesBetweenGoal,
+                Padding(
+                  padding: paddingTop,
+                  child: GoalWidget(
+                    titleGoal: 'Lead a healthy lifestyle',
+                    iconImageWidget: IconImageWidget.lifeStyleIconWay(),
+                  ),
+                ),
+                spacesBetweenGoal,
+                Padding(
+                  padding: paddingTop,
+                  child: GoalWidget(
+                    titleGoal: 'Improve digestion',
+                    iconImageWidget: IconImageWidget.digestionIconWay(),
+                  ),
+                ),
+                spacesBetweenGoal,
+                Padding(
+                  padding: paddingTop,
+                  child: GoalWidget(
+                    titleGoal: 'Other',
+                    iconImageWidget: IconImageWidget.otherIconWay(),
+                  ),
+                ),
+              ],
+            ),
+            spaces,
+            CustomButton(onPressed: () {}, text: LocaleKeys.next.tr()),
           ],
         ),
       ),
