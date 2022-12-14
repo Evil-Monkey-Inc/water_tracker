7b1bbc15d8c6a951763ec083e49fb9cd4aef85ab
Andrey <lkomarichev@gmail.com>
2022-12-14 19:00:59 +0200

    WT-88: refactored goal layout with grid view

--- lib/presentation/screens/goal_screen/goal_layout.dart
+++ lib/presentation/screens/goal_screen/goal_layout.dart
@@ -1,9 +1,10 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:water_tracker/data/models/goal_widget_model.dart';
+import 'package:water_tracker/data/models/goals.dart';
 import 'package:water_tracker/generated/locale_keys.g.dart';
 import 'package:water_tracker/presentation/widgets/custom_button.dart';
 import 'package:water_tracker/presentation/widgets/goal_widget.dart';
-import 'package:water_tracker/presentation/widgets/icon_image_widget.dart';
 import 'package:water_tracker/presentation/widgets/name_and_skip_widget.dart';
 import 'package:water_tracker/presentation/widgets/title_settings_widget.dart';
 
@@ -15,8 +16,8 @@ class GoalLayout extends StatelessWidget {
   static const paddingHorizontal = EdgeInsets.symmetric(horizontal: 24.0);
   static const spacesBetweenGoal = SizedBox(width: 16);
   static const paddingTop = EdgeInsets.only(top: 16);
-  static const upperFlex = 2;
-  static const downFlex = 2;
+  static const upperFlex = 3;
+  static const downFlex = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -37,72 +38,24 @@ class GoalLayout extends StatelessWidget {
               downFlex: downFlex,
             ),
             spaces,
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    GoalWidget(
-                      titleGoal: LocaleKeys.drink_more_water.tr(),
-                      iconImageWidget: IconImageWidget.bottleIconWay(),
-                      onChanged: (goal) {
-                        print(goal);
-                      },
-                    ),
-                    spacesBetweenGoal,
-                    GoalWidget(
-                      titleGoal: LocaleKeys.lose_weight.tr(),
-                      iconImageWidget: IconImageWidget.weightIconWay(),
-                      onChanged: (goal) {
-                        print(goal);
-                      },
-                    ),
-                  ],
-                ),
-                spacesBetween,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    GoalWidget(
-                      titleGoal: LocaleKeys.shine_skin.tr(),
-                      iconImageWidget: IconImageWidget.shineSkinIconWay(),
-                      onChanged: (goal) {
-                        print(goal);
-                      },
-                    ),
-                    spacesBetweenGoal,
-                    GoalWidget(
-                      titleGoal: LocaleKeys.lead_a_healthy_lifestyle.tr(),
-                      iconImageWidget: IconImageWidget.lifeStyleIconWay(),
-                      onChanged: (goal) {
-                        print(goal);
-                      },
-                    ),
-                  ],
-                ),
-                spacesBetween,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    GoalWidget(
-                      titleGoal: LocaleKeys.improve_digestion.tr(),
-                      iconImageWidget: IconImageWidget.digestionIconWay(),
-                      onChanged: (goal) {
-                        print(goal);
-                      },
-                    ),
-                    spacesBetweenGoal,
-                    GoalWidget(
-                      titleGoal: LocaleKeys.another.tr(),
-                      iconImageWidget: IconImageWidget.otherIconWay(),
-                      onChanged: (goal) {
-                        print(goal);
-                      },
-                    ),
-                  ],
-                ),
-              ],
+            Expanded(
+              child: GridView.count(
+                mainAxisSpacing: 12,
+                crossAxisSpacing: 12,
+                crossAxisCount: 2,
+                children: Goals.values
+                    .map(
+                      (goal) => GoalWidget(
+                        onChanged: (_) => print(goal),
+                        model: GoalWidgetModel(
+                          goal: goal,
+                          title: goal.title,
+                          imagePath: goal.assetPath,
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
             ),
             spacesBetween,
             CustomButton(onPressed: () {}, text: LocaleKeys.next.tr()),
--- lib/presentation/widgets/icon_image_widget.dart
+++ lib/presentation/widgets/icon_image_widget.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:water_tracker/assets.dart';
 
 class IconImageWidget extends StatelessWidget {
   const IconImageWidget(
     this.imagePath, {
     Key? key,
   }) : super(key: key);
-  final String imagePath;
 
-  factory IconImageWidget.bottleIconWay() => const IconImageWidget(Assets.bottleIconImage);
-  factory IconImageWidget.weightIconWay() => const IconImageWidget(Assets.weightIconImage);
-  factory IconImageWidget.shineSkinIconWay() => const IconImageWidget(Assets.starsIconImage);
-  factory IconImageWidget.lifeStyleIconWay() => const IconImageWidget(Assets.hikeIconImage);
-  factory IconImageWidget.digestionIconWay() => const IconImageWidget(Assets.carrotIconImage);
-  factory IconImageWidget.otherIconWay() => const IconImageWidget(Assets.dumbbellsIconImage);
+  final String imagePath;
 
   @override
   Widget build(BuildContext context) {
