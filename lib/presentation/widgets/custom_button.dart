c34635c8ac0f35b06a48fc25fe3cfda0b44b462e
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-16 00:04:04 +0200

    WT-89: made custom button more flexible

--- lib/presentation/widgets/custom_button.dart
+++ lib/presentation/widgets/custom_button.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:water_tracker/custom_theme.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({Key? key, required this.text, required this.onPressed, this.isEnabled = true}) : super(key: key);
+  const CustomButton(
+      {Key? key,
+      required this.text,
+      required this.onPressed,
+      this.isEnabled = true,
+      required this.buttonColor,
+      required this.textButtonColor})
+      : super(key: key);
 
   final bool isEnabled;
   final String text;
   final VoidCallback? onPressed;
+  final Color buttonColor;
+  final Color textButtonColor;
   static const paddingInsideButton = EdgeInsets.all(8.0);
   static const paddingAroundButton = EdgeInsets.symmetric(vertical: 12.0);
 
@@ -15,7 +24,9 @@ class CustomButton extends StatelessWidget {
     return ElevatedButton(
       onPressed: isEnabled ? onPressed : null,
       style: ElevatedButton.styleFrom(
-        primary: CustomTheme.buttonColor,
+        primary: buttonColor,
+        elevation: 0.0,
+        shadowColor: Colors.transparent,
         padding: paddingInsideButton,
         minimumSize: CustomTheme.minimumSizeButton,
         shape: const RoundedRectangleBorder(borderRadius: CustomTheme.roundingButton),
@@ -24,7 +35,7 @@ class CustomButton extends StatelessWidget {
         padding: paddingAroundButton,
         child: Text(
           text,
-          style: CustomTheme.buttonSize,
+          style: TextStyle(fontSize: 16, color: textButtonColor),
         ),
       ),
     );
