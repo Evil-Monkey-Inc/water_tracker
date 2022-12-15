ec6d7dfb6984cc1c38b1b754bddd459126f42f2d
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-15 17:40:39 +0200

    WT-89: add BlocProvider to GoalScreen

--- lib/presentation/screens/goal_screen/goal_screen.dart
+++ lib/presentation/screens/goal_screen/goal_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:water_tracker/data/repository/repository.dart';
+import 'package:water_tracker/get_it.dart';
+import 'package:water_tracker/presentation/screens/goal_screen/bloc/goal_bloc.dart';
 import 'goal_layout.dart';
 
 class GoalScreen extends StatelessWidget {
@@ -6,6 +10,11 @@ class GoalScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(child: GoalLayout());
+    return SafeArea(
+      child: BlocProvider<GoalBloc>(
+        create: (context) => GoalBloc(getIt<Repository>()),
+        child: const GoalLayout(),
+      ),
+    );
   }
 }
