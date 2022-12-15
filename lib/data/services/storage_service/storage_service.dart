2b13cccd84a0aa3838d63a29df64798aefc721d3
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-15 15:39:13 +0200

    WT-88: created service for saving user goal

--- lib/data/services/storage_service/storage_service.dart
+++ lib/data/services/storage_service/storage_service.dart
@@ -1,5 +1,8 @@
+import 'package:water_tracker/data/models/goal_list.dart';
 import 'package:water_tracker/data/models/user_settings.dart';
 
 abstract class StorageService {
   Future<bool> saveGeneralInfo(UserSettings userSettings);
+
+  Future<bool> saveGoal(GoalsList goalsList);
 }
--- lib/data/services/storage_service/storage_service_shered_preff_impl.dart
+++ lib/data/services/storage_service/storage_service_shered_preff_impl.dart
@@ -1,16 +1,25 @@
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:water_tracker/data/models/goal_list.dart';
 import 'package:water_tracker/data/models/user_settings.dart';
 import 'package:water_tracker/data/services/storage_service/storage_service.dart';
 
 class StorageServiceSharedPrefImplements extends StorageService {
   static const userSettingsKey = 'userSettings';
+  static const goalsListKey = 'goalsListKey';
 
   @override
   Future<bool> saveGeneralInfo(UserSettings userSettings) async {
     final prefs = await SharedPreferences.getInstance();
     final result = await prefs.setString(userSettingsKey, jsonEncode(userSettings.toJson()));
 
+    return result;
+  }
+
+  @override
+  Future<bool> saveGoal(GoalsList goalsList) async {
+    final prefs = await SharedPreferences.getInstance();
+    final result = await prefs.setString(goalsListKey, jsonEncode(goalsList.toJson()));
     return result;
   }
 }
