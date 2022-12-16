804aae90cc78180b3ce059ae1d1ea51171e517b2
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-16 02:36:25 +0200

    WT-96: created greeting layout

---
+++ lib/presentation/screens/greeting_screen/greeting_layout.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:water_tracker/presentation/widgets/greeting_widget..dart';
+
+class GreetingLayout extends StatelessWidget {
+  const GreetingLayout({Key? key}) : super(key: key);
+
+  static const paddingHorizontal = EdgeInsets.symmetric(horizontal: 28.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: paddingHorizontal,
+        child: Column(
+          children: [GreetingWidget()],
+        ),
+      ),
+    );
+  }
+}
