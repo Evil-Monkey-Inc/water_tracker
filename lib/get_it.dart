f5dde9b862dd46acbe736478c27c6d78dd1cf059
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-12-20 00:16:34 +0200

    WT-97: added LocalNotificationService to dependencies

--- lib/get_it.dart
+++ lib/get_it.dart
@@ -3,6 +3,7 @@ import 'package:water_tracker/data/repository/repository.dart';
 import 'package:water_tracker/data/repository/repository_impl.dart';
 import 'package:water_tracker/data/services/authentication_service/authentication_service.dart';
 import 'package:water_tracker/data/services/authentication_service/authentication_service_firebase_impl.dart';
+import 'package:water_tracker/data/services/notification_service/local_notification_service_impl.dart';
 import 'package:water_tracker/data/services/storage_service/storage_service.dart';
 import 'package:water_tracker/data/services/storage_service/storage_service_shered_preff_impl.dart';
 
@@ -11,7 +12,8 @@ final getIt = GetIt.instance;
 void setupServicesLocator() {
   final authService = AuthenticationServiceFirebaseImpl();
   final storageService = StorageServiceSharedPrefImplements();
+  final notificationService = LocalNotificationServiceImpl();
   getIt.registerSingleton<AuthenticationService>(authService);
   getIt.registerSingleton<StorageService>(storageService);
-  getIt.registerSingleton<Repository>(RepositoryImpl(authService, storageService));
+  getIt.registerSingleton<Repository>(RepositoryImpl(authService, storageService, notificationService));
 }
