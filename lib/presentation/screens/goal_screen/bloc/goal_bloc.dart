fba5319a7b382538a1d0acadc55d881410245a8e
georg <georgii.gabaidze@gmail.com>
2023-01-25 00:36:25 +0200

    WT-100: passed email from repo saveGoal, saveCupCount and getCupCount methods

--- lib/presentation/screens/goal_screen/bloc/goal_bloc.dart
+++ lib/presentation/screens/goal_screen/bloc/goal_bloc.dart
@@ -1,21 +1,23 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:water_tracker/data/models/goal_list.dart';
 import 'package:water_tracker/data/repository/repository.dart';
+import 'package:water_tracker/data/repository/repository_impl.dart';
 import 'package:water_tracker/presentation/screens/goal_screen/bloc/goal_event.dart';
 import 'package:water_tracker/presentation/screens/goal_screen/bloc/goal_state.dart';
 
 class GoalBloc extends Bloc<GoalEvent, GoalState> {
-  GoalBloc(this.repository) : super(InitialGoalState()) {
+  GoalBloc(this.repository, [this.repositoryImpl]) : super(InitialGoalState()) {
     on<SaveGoalEvent>((event, emit) async {
       try {
         emit(SavingGoalState());
         final goals = GoalList(goals: event.goals);
-        final isSuccess = await repository.saveGoal(event.email, goals);
+        final isSuccess = await repository.saveGoal(repositoryImpl!.userEmail ,goals);
         emit(isSuccess ? SuccessfullyGoalState() : ErrorGoalState(Exception()));
       } catch (e) {
         emit(ErrorGoalState(e));
       }
     });
   }
+  final RepositoryImpl? repositoryImpl;
   final Repository repository;
 }
--- lib/presentation/screens/main_screen/bloc/main_screen_bloc.dart
+++ lib/presentation/screens/main_screen/bloc/main_screen_bloc.dart
@@ -1,22 +1,23 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:water_tracker/data/repository/repository.dart';
+import 'package:water_tracker/data/repository/repository_impl.dart';
 import 'package:water_tracker/presentation/screens/main_screen/bloc/main_screen_event.dart';
 import 'package:water_tracker/presentation/screens/main_screen/bloc/main_screen_state.dart';
 
 class MainScreenBloc extends Bloc<MainScreenEvent, MainScreenState> {
-  MainScreenBloc(this.repository) : super(CounterState(0)) {
+  MainScreenBloc(this.repository, [this.repositoryImpl]) : super(CounterState(0)) {
     on<SaveCounterEvent>((event, emit) async {
       try {
         final newCount = state.counter + 1;
         if (newCount > maxCupCount) return;
-        final isSaved = await repository.saveCupCount(event.email, newCount);
+        final isSaved = await repository.saveCupCount(repositoryImpl!.userEmail, newCount);
         emit(isSaved ? CounterState(newCount) : ErrorMainScreenState(state.counter));
       } catch (e) {
         ErrorMainScreenState(state.counter);
       }
     });
     on<AppLaunchEvent>((event, emit) async {
-      final result = await repository.getCupCount(event.email, DateTime.now());
+      final result = await repository.getCupCount(repositoryImpl!.userEmail, DateTime.now());
       emit(result == null ? ErrorMainScreenState(state.counter) : CounterState(result));
     });
   }
@@ -25,5 +26,6 @@ class MainScreenBloc extends Bloc<MainScreenEvent, MainScreenState> {
 
   static const singleCupWeight = 250;
 
+  final RepositoryImpl? repositoryImpl;
   final Repository repository;
 }
