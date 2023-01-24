166f0792370b7f404b5cb8ee0e8d5d3fcb466813
georg <georgii.gabaidze@gmail.com>
2023-01-25 00:35:06 +0200

    WT-100: saved email to repo from bloc

--- lib/presentation/screens/sign_up_screen/bloc/sign_up_bloc.dart
+++ lib/presentation/screens/sign_up_screen/bloc/sign_up_bloc.dart
@@ -1,14 +1,16 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:water_tracker/data/repository/repository.dart';
+import 'package:water_tracker/data/repository/repository_impl.dart';
 import 'package:water_tracker/presentation/screens/sign_up_screen/bloc/sign_up_event.dart';
 import 'package:water_tracker/presentation/screens/sign_up_screen/bloc/sign_up_state.dart';
 
 class SignUpBloc extends Bloc<SignUpEvent, SignUpState> {
-  SignUpBloc(this.repository) : super(InitialSignUpState()) {
+  SignUpBloc(this.repository, [this.repositoryImpl]) : super(InitialSignUpState()) {
     on<CreateUserEvent>((event, emit) async {
       try {
         emit(LoadingSignUpState());
         final isSuccess = await repository.registerUser(event.password, event.email);
+        repositoryImpl!.userEmail = event.email;
         emit(isSuccess ? SuccessfullySignUpState() : ErrorRegistrationState(Exception()));
       } catch (e) {
         emit(ErrorRegistrationState(e));
@@ -16,4 +18,5 @@ class SignUpBloc extends Bloc<SignUpEvent, SignUpState> {
     });
   }
   final Repository repository;
+  final RepositoryImpl? repositoryImpl;
 }
