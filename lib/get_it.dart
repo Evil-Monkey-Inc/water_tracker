8fe02be4f1032e20bfc6f2de65c3fbd9871786a6
georg <georgii.gabaidze@gmail.com>
2023-01-31 18:19:44 +0200

    WT-100: change implementation

--- lib/get_it.dart
+++ lib/get_it.dart
@@ -4,8 +4,8 @@ import 'package:water_tracker/data/repository/repository_impl.dart';
 import 'package:water_tracker/data/services/authentication_service/authentication_service.dart';
 import 'package:water_tracker/data/services/authentication_service/authentication_service_firebase_impl.dart';
 import 'package:water_tracker/data/services/authentication_service/mocked_authentication_service_impl.dart';
-import 'package:water_tracker/data/services/storage_service/firestore_storage_service.dart';
-import 'package:water_tracker/data/services/storage_service/firestore_storage_service_impl.dart';
+import 'package:water_tracker/data/services/storage_service/shared_preff_storage_service.dart';
+import 'package:water_tracker/data/services/storage_service/shared_preff_storage_service_impl.dart';
 import 'package:water_tracker/env_variables.dart';
 
 final getIt = GetIt.instance;
@@ -14,10 +14,10 @@ void setupServicesLocator() {
   final authService = EnvVariables.mockAuthService
       ? MockedAuthenticationServiceImpl()
       : AuthenticationServiceFirebaseImpl();
-  final firebaseStorage = FireStoreStorageServiceImpl();
+  final firebaseStorage = SharedPreffStorageImplements();
 
   getIt.registerSingleton<AuthenticationService>(authService);
-  getIt.registerSingleton<FireStoreStorageService>(firebaseStorage);
+  getIt.registerSingleton<SharedPreffStorageService>(firebaseStorage);
   getIt.registerSingleton<Repository>(
     RepositoryImpl(authService, firebaseStorage),
   );
