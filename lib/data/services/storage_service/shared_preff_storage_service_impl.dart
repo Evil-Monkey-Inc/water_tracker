d8a95617a0e5d30f8966750b4ad3f8c2458ef1db
georg <georgii.gabaidze@gmail.com>
2023-02-01 14:10:34 +0200

    WT-100: refactored nesting of shared preff

--- lib/data/services/storage_service/shared_preff_storage_service_impl.dart
+++ lib/data/services/storage_service/shared_preff_storage_service_impl.dart
@@ -3,46 +3,34 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:water_tracker/data/models/goal_list.dart';
 import 'package:water_tracker/data/models/user_settings.dart';
 import 'package:water_tracker/data/services/storage_service/shared_preff_storage_service.dart';
+import 'package:water_tracker/data/services/storage_service/storage_service.dart';
 
 class SharedPreffStorageImplements extends SharedPreffStorageService {
-
-  static const amountOfWaterDrunkKey = 'amountOfWaterDrunk';
-  static const userCountKey = 'userCount';
-
-
-  static const exceptionMessage = 'email could not be null at this point';
-  final List<String> userInfo = [];
-
   @override
   Future<bool> saveUserSetting(String email, UserSettings userSettings) async {
     final prefs = await SharedPreferences.getInstance();
     final result = await prefs.setString(
       email,
-      jsonEncode(userSettings),
+      jsonEncode(
+        {StorageService.userSettingsKey: jsonEncode(userSettings), StorageService.userGoalKey: []},
+      ),
     );
-
-    final encodeMap = prefs.getString(email);
-    userInfo.add(encodeMap!);
-
     return result;
   }
 
   @override
   Future<bool> saveUserGoal(String email, GoalList goalsList) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setString(email, jsonEncode(goalsList));
-    final encodeMap = prefs.getString(email);
-    userInfo.add(encodeMap!);
-    final result = await prefs.setStringList(email, userInfo);
+    final result = await prefs.setStringList(StorageService.userGoalKey, [jsonEncode(goalsList)]);
     return result;
   }
 
   @override
   Future<bool> saveUserCount(String email, String dateKey, int counterCups) async {
     final prefs = await SharedPreferences.getInstance();
-    final cupsMap = {dateKey : counterCups};
-    final fullNestingMap = {userCountKey : cupsMap};
-    final result = await prefs.setString(amountOfWaterDrunkKey, jsonEncode(fullNestingMap));
+    final cupsMap = {dateKey: counterCups};
+    final fullNestingMap = {StorageService.userCountKey: cupsMap};
+    final result = await prefs.setString(StorageService.amountOfWaterDrunkKey, jsonEncode(fullNestingMap));
     return result;
   }
 
