517d54e10d7afe617e741bcd913baf3938f81c15
Benik <galstyan.benik@gmail.com>
2023-04-25 12:59:27 +0300

    removed unused variables

--- lib/custom_theme.dart
+++ lib/custom_theme.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:water_tracker/generated/assets/assets.gen.dart';
 
 class CustomTheme {
 
@@ -12,15 +10,4 @@ class CustomTheme {
   static const minimumSizeButton = Size.fromHeight(8);
 
   static const roundingButton = BorderRadius.all(Radius.circular(40));
-
-  static const backgroundColor = Color(0xFFFAFAFA);
-
-
-
-  static const systemTheme = SystemUiOverlayStyle(
-    statusBarColor: backgroundColor,
-    systemNavigationBarColor: backgroundColor,
-    statusBarIconBrightness: Brightness.dark, // For Android (dark icons)
-    statusBarBrightness: Brightness.light, // For iOS (dark icons)
-  );
 }
