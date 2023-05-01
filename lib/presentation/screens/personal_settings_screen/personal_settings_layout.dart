843870ab06580e78d16109e288a3f1fda3272133
Benik <galstyan.benik@gmail.com>
2023-05-01 17:19:54 +0300

    deleted unnecessary import

--- lib/presentation/screens/personal_settings_screen/personal_settings_layout.dart
+++ lib/presentation/screens/personal_settings_screen/personal_settings_layout.dart
@@ -1,7 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:water_tracker/custom_theme.dart';
 import 'package:water_tracker/data/models/gender.dart';
 import 'package:water_tracker/generated/locale_keys.g.dart';
 import 'package:water_tracker/presentation/screens/goal_screen/goal_screen.dart';
