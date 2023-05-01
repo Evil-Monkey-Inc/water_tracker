c8aaea4411cc8339c2e5ffc67d0df8c419cc4205
Benik <galstyan.benik@gmail.com>
2023-05-01 22:33:43 +0300

    formatted code and created boolean variable for conditional operator

--- lib/presentation/widgets/molecules/custom_button.dart
+++ lib/presentation/widgets/molecules/custom_button.dart
@@ -1,51 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:water_tracker/config/theme/theme_extension/radius_extension.dart';
 import 'package:water_tracker/custom_theme.dart';
 import 'package:water_tracker/generated/assets/assets.gen.dart';
 
 class CustomButton extends StatelessWidget {
   const CustomButton({
     super.key,
-    required this.text,
-    required this.onPressed,
-    this.isEnabled = true,
-    required this.buttonColor,
-    required this.textButtonColor,
     this.icon,
     this.image,
+    required this.text,
+    this.isEnabled = true,
+    required this.onPressed,
+    required this.buttonColor,
+    required this.textButtonColor,
   });
 
-  final bool isEnabled;
   final String text;
-  final VoidCallback? onPressed;
+  final Widget? image;
+  final IconData? icon;
+  final bool isEnabled;
   final Color buttonColor;
   final Color textButtonColor;
-  final IconData? icon;
-  final Widget? image;
-  static const paddingAroundButton = EdgeInsets.symmetric(vertical: 12.0);
-  static const paddingIcon = EdgeInsets.only(right: 10);
-  static const paddingImage = EdgeInsets.only(left: 1.0, right: 3.0);
+  final VoidCallback? onPressed;
+
+  static const elevation = 0.0;
   static const iconSize = 20.0;
+
   static const emptySpace = SizedBox();
   static const spaces = SizedBox(width: 8.0);
-  static const elevation = 0.0;
+  static const paddingButton = EdgeInsets.all(8.0);
+  static const paddingIcon = EdgeInsets.only(right: 10);
+  static const paddingImage = EdgeInsets.only(left: 1.0, right: 3.0);
+  static const paddingAroundButton = EdgeInsets.symmetric(vertical: 12.0);
 
   @override
   Widget build(BuildContext context) {
+    final hasImage = image != null;
     final buttonStyleProp = TextStyle(
       fontSize: CustomTheme.fontSizeButton,
       color: Theme.of(context).backgroundColor,
       fontFamily: Assets.fonts.senBold,
     );
-    final a =
-        Theme.of(context).extension<RadiusExtension>()?.buttonBorderRadius;
     return ElevatedButton(
       onPressed: isEnabled ? onPressed : null,
       style: ElevatedButton.styleFrom(
         backgroundColor: buttonColor,
         elevation: elevation,
         shadowColor: Colors.transparent,
-        padding: EdgeInsets.all(a!),
+        padding: paddingButton,
         minimumSize: CustomTheme.minimumSizeButton,
         shape: const RoundedRectangleBorder(
           borderRadius: CustomTheme.roundingButton,
@@ -55,15 +56,16 @@ class CustomButton extends StatelessWidget {
         padding: paddingAroundButton,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
             if (icon != null)
               Padding(
                 padding: paddingIcon,
                 child: Icon(icon),
               ),
-            image != null
-                ? Padding(padding: paddingImage, child: image!)
-                : emptySpace,
+            if (hasImage == true)
+              Padding(padding: paddingImage, child: image!)
+            else
+              emptySpace,
             spaces,
             Text(
               text,
