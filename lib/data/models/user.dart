3c81565a848bb2447eca0e73019c8ba49b2e01ff
george <heorhii.habaidze@oneplusone.solutions>
2023-05-05 15:21:08 +0300

    made json methods for user model

--- lib/data/models/user.dart
+++ lib/data/models/user.dart
@@ -1,8 +1,21 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'user.g.dart';
+
+@JsonSerializable()
 class User {
   User(
     this.email, {
     this.username,
   });
+
+
+  factory User.fromJson(Map<String, dynamic> json) =>
+      _$UserFromJson(json);
   final String email;
   final String? username;
+
+
+  Map<String, dynamic> toJson() => _$UserToJson(this);
 }
+
+
