88bd3cdd5cc85de43dad08997af7830a230ed57d
george <heorhii.habaidze@oneplusone.solutions>
2023-05-05 15:19:36 +0300

    added new methods for SharedPreffStorageService

--- lib/data/services/storage_service/shared_preff_storage_service_impl.dart
+++ lib/data/services/storage_service/shared_preff_storage_service_impl.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:water_tracker/data/models/goal_list.dart';
+import 'package:water_tracker/data/models/user.dart';
 import 'package:water_tracker/data/models/user_settings.dart';
 import 'package:water_tracker/data/services/storage_service/shared_preff_storage_service.dart';
 
 class SharedPreffStorageImplements extends SharedPreffStorageService {
   static const userSettingsKey = 'userSettings';
   static const goalsListKey = 'goalsListKey';
+  static const userInfo = 'userInfoKey';
 
   @override
   Future<bool> saveGeneralInfo(UserSettings userSettings) async {
@@ -44,5 +46,18 @@ class SharedPreffStorageImplements extends SharedPreffStorageService {
   Future<int?> getCupCount(String dateKey) async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getInt(dateKey) ?? 0;
+  }
+
+  @override
+  Future<void> saveUserInfo(String email, String? username) async {
+    final prefs = await SharedPreferences.getInstance();
+    final user = User(email, username: username);
+    await prefs.setString(userInfo, jsonEncode(user.toJson()));
+  }
+
+  @override
+  Future<String?> getUserInfo() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(userInfo);
   }
 }
