ad4ff6a1a21993df46d8639e4dd1d0474c2aeab6
Benik <galstyan.benik@gmail.com>
2023-05-08 18:06:32 +0300

    formatted code , created saveGoogleInfo method for SharedPreffStorageService

--- lib/data/services/storage_service/shared_preff_storage_service.dart
+++ lib/data/services/storage_service/shared_preff_storage_service.dart
@@ -2,17 +2,19 @@ import 'package:water_tracker/data/models/goal_list.dart';
 import 'package:water_tracker/data/models/user_settings.dart';
 
 abstract class SharedPreffStorageService {
-  Future<bool> saveGeneralInfo(UserSettings userSettings);
-
-  Future<bool> saveGoal(GoalList goalsList);
-
   Future<String?> getGoal();
 
-  Future<bool> saveCupCount(String dateKey, int counterCups);
+  Future<String?> getUserInfo();
 
   Future<int?> getCupCount(String dateKey);
 
+  Future<bool> saveGoal(GoalList goalsList);
+
+  Future<void> saveGoogleInfo(String email);
+
+  Future<bool> saveGeneralInfo(UserSettings userSettings);
+
   Future<void> saveUserInfo(String email, String? username);
 
-  Future<String?> getUserInfo();
+  Future<bool> saveCupCount(String dateKey, int counterCups);
 }
