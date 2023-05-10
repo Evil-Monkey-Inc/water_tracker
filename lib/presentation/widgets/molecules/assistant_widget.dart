09fab5caef6550e6cb46e37e8e7ee64760f91b62
andrey <lkomarichev@gmail.com>
2023-05-10 14:37:50 +0300

    reused for general layout

--- lib/presentation/widgets/molecules/assistant_widget.dart
+++ lib/presentation/widgets/molecules/assistant_widget.dart
@@ -1,29 +1,29 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:water_tracker/generated/assets/assets.gen.dart';
-import 'package:water_tracker/presentation/widgets/atoms/calendar_widget.dart';
+import 'package:water_tracker/presentation/screens/general_settings_screen/general_settings_screen.dart';
 import 'package:water_tracker/presentation/widgets/atoms/custom_icon_button_widget.dart';
 
 class AssistantWidget extends StatefulWidget {
-  const AssistantWidget({super.key});
+  const AssistantWidget({super.key, required this.test});
+
+  final Widget test;
 
   @override
   State<AssistantWidget> createState() => _AssistantWidgetState();
 }
 
 class _AssistantWidgetState extends State<AssistantWidget> {
-  final dataNow = DateTime.now();
-  static const dateFormat = 'EEE, d';
-
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         const Spacer(),
-        CalendarWidget(dateTime: DateFormat(dateFormat).format(dataNow)),
+        widget.test,
         const Spacer(),
         CustomIconButtonWidget(
-          onTap: () {},
+          onTap: () {
+            Navigator.of(context).pushNamed(GeneralSettingsScreen.route);
+          },
           image: Image.asset(Assets.images.settingIcon.path),
         ),
       ],
