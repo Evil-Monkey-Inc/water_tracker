68074d252f3637021015bb70214bb695deee85cd
andrey <lkomarichev@gmail.com>
2023-05-10 14:55:24 +0300

    rebuilt this widget to be able to use the select_sex_button on another screen

--- lib/presentation/screens/personal_settings_screen/personal_settings_layout.dart
+++ lib/presentation/screens/personal_settings_screen/personal_settings_layout.dart
@@ -1,7 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:water_tracker/custom_theme.dart';
 import 'package:water_tracker/data/models/gender.dart';
 import 'package:water_tracker/generated/locale_keys.g.dart';
 import 'package:water_tracker/presentation/screens/goal_screen/goal_screen.dart';
@@ -22,18 +21,20 @@ class PersonalSettingsLayout extends StatefulWidget {
 }
 
 class _PersonalSettingsLayoutState extends State<PersonalSettingsLayout> {
-  var gender = Gender.male;
-  int weight = minValueWeight;
+  Gender gender = Gender.male;
+
   int age = minValueAge;
+  int weight = minValueWeight;
+
+  static const downFlex = 1;
+  static const upperFlex = 3;
+  static const fontSize = 16.0;
+  static const minValueAge = 5;
+  static const maxValueAge = 100;
+  static const minValueWeight = 24;
+  static const maxValueWeight = 150;
   static const spaces = SizedBox(height: 14);
   static const paddingHorizontal = EdgeInsets.symmetric(horizontal: 24.0);
-  static const maxValueAge = 100;
-  static const minValueAge = 5;
-  static const maxValueWeight = 150;
-  static const minValueWeight = 24;
-  static const fontSize = 16.0;
-  static const upperFlex = 3;
-  static const downFlex = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -64,9 +65,7 @@ class _PersonalSettingsLayoutState extends State<PersonalSettingsLayout> {
                 children: [
                   spaces,
                   NameAndSkipWidget(
-                    onPressed: () => context
-                        .read<PersonalSettingBloc>()
-                        .add(SkipPersonalSettingScreenEvent()),
+                    onPressed: () => context.read<PersonalSettingBloc>().add(SkipPersonalSettingScreenEvent()),
                   ),
                   spaces,
                   TitleSettingWidget(
@@ -77,7 +76,16 @@ class _PersonalSettingsLayoutState extends State<PersonalSettingsLayout> {
                   spaces,
                   spaces,
                   SelectSexButton(
-                    onChanged: (value) => setState(() => gender = value),
+                    onChanged: (value) {
+                      setState(() {
+                        gender = value;
+                        print(value);
+                      });
+                    },
+                    firstTabTitle: LocaleKeys.man.tr(),
+                    secondTabTitle: LocaleKeys.woman.tr(),
+                    aboutTabBar: LocaleKeys.sex.tr(),
+                    data: gender,
                   ),
                   CustomSliderWidget(
                     onChanged: (value) => setState(() => age = value),
