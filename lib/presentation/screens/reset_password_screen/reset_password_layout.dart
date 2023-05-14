176d36af869ed39bdc55dab65a2072f77806cbf4
Benik <galstyan.benik@gmail.com>
2023-05-15 02:15:49 +0300

    Implementation of created widgets for layout

--- lib/presentation/screens/reset_password_screen/reset_password_layout.dart
+++ lib/presentation/screens/reset_password_screen/reset_password_layout.dart
@@ -1,10 +1,13 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:water_tracker/generated/locale_keys.g.dart';
 import 'package:water_tracker/presentation/screens/reset_password_screen/bloc/reset_password_bloc.dart';
-import 'package:water_tracker/presentation/screens/reset_password_screen/bloc/reset_password_event.dart';
 import 'package:water_tracker/presentation/screens/reset_password_screen/bloc/reset_password_state.dart';
-import 'package:water_tracker/presentation/widgets/molecules/custom_button.dart';
-import 'package:water_tracker/presentation/widgets/molecules/input_field_widget.dart';
+import 'package:water_tracker/presentation/widgets/atoms/calendar_widget.dart';
+import 'package:water_tracker/presentation/widgets/atoms/title_settings_widget.dart';
+import 'package:water_tracker/presentation/widgets/molecules/assistant_widget.dart';
+import 'package:water_tracker/presentation/widgets/organisms/reset_password_form.dart';
 
 class ResetPasswordLayout extends StatefulWidget {
   const ResetPasswordLayout({super.key});
@@ -14,46 +17,55 @@ class ResetPasswordLayout extends StatefulWidget {
 }
 
 class _ResetPasswordLayoutState extends State<ResetPasswordLayout> {
-  final _emailController = TextEditingController();
+  static const downFlex = 1;
+  static const upperFlex = 3;
+
+  static const space = SizedBox(height: 14);
+  static const paddingHorizontal = EdgeInsets.symmetric(horizontal: 24.0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocConsumer<ResetPasswordBloc, ResetPasswordState>(
-        listener: (context, state) {
-         if(state is ErrorResetPasswordState){
-           // TODO(Benik): Implement alert with exception
-         }
-         if(state is SuccessfullyResetPasswordState){
-           // TODO(Benik): Do something if state is SuccessfullyResetPasswordState
-         }
-        },
-        builder: (context, state) {
-          return Column(
-            children: [
-              const SizedBox(height: 220,),
-              const Center(
-                child: Text('Reset Password'),
-              ),
-              InputFieldWidget(
-                labelText: 'Enter email',
-                //  validator: FormValidators.emailValidator,
-                controller: _emailController,
-              ),
-              const SizedBox(height: 24.0,),
-              CustomButton(
-                text: 'reset password',
-                onPressed: () {
-                  context.read<ResetPasswordBloc>().add(
-                        ForgotPasswordEvent(_emailController.text),
-                      );
-                },
-                buttonColor: Colors.blue,
-                textButtonColor: Colors.black,
-              )
-            ],
-          );
-        },
+      body: Padding(
+        padding: paddingHorizontal,
+        child: BlocConsumer<ResetPasswordBloc, ResetPasswordState>(
+          listener: (context, state) {
+            if (state is ErrorResetPasswordState) {
+              // TODO(Benik): Implement alert with exception
+            }
+            if (state is SuccessfullyResetPasswordState) {
+              // TODO(Benik): Do something if state is SuccessfullyResetPasswordState
+            }
+          },
+          builder: (context, state) {
+            return Column(
+              children: [
+                space,
+                AssistantWidget(
+                  title: CalendarWidget(
+                    dateTime: LocaleKeys.reset_password.tr(),
+                  ),
+                  iconButton: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.close),
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+                space,
+                TitleSettingWidget(
+                  LocaleKeys.forgot_your_password.tr(),
+                  subTitle: LocaleKeys.enter_the_address.tr(),
+                  upperFlex: upperFlex,
+                  downFlex: downFlex,
+                ),
+                space,
+                ResetPasswordForm(
+                  onResetPasswordPressed: () {},
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
