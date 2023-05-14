09ea5e9ea49ddb69e40c6ce72a4939751081fe84
Benik <galstyan.benik@gmail.com>
2023-05-15 02:16:53 +0300

    created title and icon button

--- lib/presentation/widgets/molecules/assistant_widget.dart
+++ lib/presentation/widgets/molecules/assistant_widget.dart
@@ -1,31 +1,32 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:water_tracker/generated/assets/assets.gen.dart';
-import 'package:water_tracker/presentation/widgets/atoms/calendar_widget.dart';
-import 'package:water_tracker/presentation/widgets/atoms/custom_icon_button_widget.dart';
 
 class AssistantWidget extends StatefulWidget {
-  const AssistantWidget({super.key});
+  const AssistantWidget({
+    super.key,
+    required this.title,
+    required this.iconButton,
+  });
+
+  final Widget title;
+  final Widget iconButton;
 
   @override
   State<AssistantWidget> createState() => _AssistantWidgetState();
 }
 
 class _AssistantWidgetState extends State<AssistantWidget> {
-  final dataNow = DateTime.now();
-  static const dateFormat = 'EEE, d';
-
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        const Spacer(),
-        CalendarWidget(dateTime: DateFormat(dateFormat).format(dataNow)),
-        const Spacer(),
-        CustomIconButtonWidget(
-          onTap: () {},
-          image: Image.asset(Assets.images.settingIcon.path),
+        const Spacer(
+          flex: 3,
         ),
+        Center(child: widget.title),
+        const Spacer(
+          flex: 2,
+        ),
+        widget.iconButton,
       ],
     );
   }
