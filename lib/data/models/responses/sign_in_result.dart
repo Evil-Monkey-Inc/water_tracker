ce6bfbfe966e8bd21e1d7f2668617dec5964cc7c
Benik <galstyan.benik@gmail.com>
2023-05-30 21:00:11 +0300

    created token  for sign in result

--- lib/data/models/responses/sign_in_result.dart
+++ lib/data/models/responses/sign_in_result.dart
@@ -2,7 +2,8 @@ import 'package:water_tracker/data/models/errors/sign_up_exception.dart';
 import 'package:water_tracker/data/models/user.dart';
 
 class SignInResult {
-  SignInResult(this.user, this.error);
+  SignInResult(this.user, this.error, this.token);
   final User? user;
+  final String? token;
   final SignUpException? error;
 }
